007f913d3d2a301f4934557f75e1b5c345a37cf5
L1quide <L1quid_e@inbox.ru>
2020-03-05 04:21:59 +0700

    get_url function init

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import requests
 from requests.exceptions import HTTPError
+from requests.auth import HTTPBasicAuth
 from bs4 import BeautifulSoup as bs
 import csv
 def user_face():
@@ -7,25 +8,73 @@ def user_face():
 
 url = 'https://novosibirsk.hh.ru/search/vacancy?area=1&st=searchVacancy&text=Python+junior&from=suggest_post'
 headers = {'accept':'*/*','user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) snap Chromium/80.0.3987.87 Chrome/80.0.3987.87 Safari/537.36'}
+def get_url(*args, **kwargs):
+    pass
+
 def get_html(*args, **kwargs):
-    result = requests.get(url, headers=headers)
+    session = requests.Session()
+    result = session.get(url, headers=headers, auth=('79537809733','Xervam__13312'))
     try:
         result.status_code == 200
         print('ok')
-        print(result)
+        return result.text
     except:
-        print('not')
-
-get_html(url, headers)
+        print('Error connect to site')
 
 
+def get_page_data(html):
+    soup = bs(html, 'lxml')
+    slise = soup.find_all(attrs={'data-qa':'vacancy-serp__vacancy'})
+    for chip in slise:
+        try:
+            name = chip.find(attrs={'data-qa':'vacancy-serp__vacancy-title'}).text
+            print(name)
+        except:
+            name = ''
+        try:
+            linck = chip.find(attrs={'data-qa': 'vacancy-serp__vacancy-title'}).get('href')
+        except:
+            linck = ''
+        data = {'name': name, 'linck': linck}
+        writer(data)
+
+def writer(data):
+    with open('resul', 'a') as f:
+        writer = csv.writer(f)
+        writer.writerow((data['name'],data['linck']))
+
+def main(*args, **kwargs):
+
+    get_page_data(get_html(url, headers))
 
 
 
 
 
-def parser():
-    pass
 
-def writer():
-    pass
+
+
+
+
+
+
+
+
+
+# def page_counter(html):
+#     soup = bs(html, 'lxml')
+#     page = soup.find_all(attrs={'data-qa':'vacancy-serp__vacancy'})
+#     i = 0
+#     length = len(page)
+#     while length > 0:
+#         i += 1
+#         payload['page'] = str(i)
+#         length -= 1
+#         session = requests.Session()
+#         result = session.get(url, headers=headers, auth=('79537809733', 'Xervam__13312'), params=payload)
+#         print(result.url)
+#     return result
+
+
+if __name__ == '__main__':
+    main()
